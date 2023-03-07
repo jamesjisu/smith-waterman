16b5cc7220dcdab5cbb032395adf7bb7a012cd62
James <jamesjisu@gmail.com>
2023-03-06 22:04:06 -0500

    update

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='smith-waterman',
-    version='0.0.3',
-    author='Mike Huls',
+    version='0.0.1',
+    author='James Han',
     author_email='jamesjisu@gmail.com',
     description='Smith-Waterman Implementation',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/jamesjisu/smith-waterman',
     license='MIT',
-    packages=['smith-waterman'],
+    packages=['smithwaterman'],
     install_requires=['numpy', 'pandas'],
 )
